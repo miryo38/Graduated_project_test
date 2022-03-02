424d20a46a1a395b8dd132ecaed744805d04ad00
김시훈 <thrchtlgns@naver.com>
2022-03-02 15:19:35 +0900

    시훈추가0302

--- src/screens/ChatScreen/ChatScreen.js
+++ src/screens/ChatScreen/ChatScreen.js
@@ -1,22 +1,52 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
-import AppText from '../../components/AppText';
-import Screen from '../../components/Screen';
+import React, { useState, useCallback, useEffect } from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat'
+const ChatScreen= () => {
+  const [messages, setMessages] = useState([]);
+ useEffect(() => {
+    setMessages([
+         {
+            _id: 0, 
+            text: '부적절하거나 불쾌감을 줄 수 있는 대화는 삼가 부탁드립니다.', 
+            createdAt: new Date().getTime(), 
+            system: true, 
+          },
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+      {
+        _id: 2,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 1,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ])
+  }, [])
 
-function ChatScreen(props) {
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+  }, [])
   return (
-    <Screen style={styles.screen}>
-      <AppText>ChatScreen</AppText>
-    </Screen>
-  );
-}
+    
+    <GiftedChat
+    messages={messages}
+    onSend={messages => onSend(messages)}
+    user={{
+      _id: 1,
+    }}
+  />
+    );
+  };
 
-const styles = StyleSheet.create({
-  screen: {
-    backgroundColor: '#f0d507',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 export default ChatScreen;
